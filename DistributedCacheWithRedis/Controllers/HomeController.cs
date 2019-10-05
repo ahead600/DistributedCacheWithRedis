c89f43f2ad5b0d372782db48f6fd0c0e52a06c97
admin <admin@DESKTOP-LSPSTM8>
2019-10-05 16:24:34 +0330

    Add project files.

---
+++ DistributedCacheWithRedis/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using DistributedCacheWithRedis.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedCacheWithRedis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDistributedCache _cache;
+
+        public HomeController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddHours(1)
+            };
+
+            await _cache.SetAsync("TestString", Encoding.ASCII.GetBytes("TestValue"), options);
+
+            var value = _cache.Get("TestString");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DistributedCacheWithRedis/Infra/IDistributedCache.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Threading.Tasks;
+
+namespace DistributedCacheWithRedis
+{
+    public interface IDistributedCache
+    {
+        byte[] Get(string key);
+        Task<byte[]> GetAsync(string key);
+
+        void Set(string key, byte[] value, DistributedCacheEntryOptions options);
+        Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options);
+
+        void Refresh(string key);
+        Task RefreshAsync(string key);
+
+        void Remove(string key);
+        Task RemoveAsync(string key);
+    }
+
+
+}
